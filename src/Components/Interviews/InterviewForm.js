65b7653dbd1431495b71f27bad50794d7d27cd4d
May Jawdat <may.jawdat@gmail.com>
2017-12-04 17:28:53 -0800

    Update structure and input types of interview form, add company form

--- src/Components/Interviews/InterviewForm.js
+++ src/Components/Interviews/InterviewForm.js
@@ -5,46 +5,67 @@ import axios from 'axios'
 class InterviewForm extends Component {
   render() {
     return (
-      <div className="New-Interview">
-          Job Title:
+      <form className="New-Interview">
+          Position:
           <input type="text" name="jobTitle"/><br/>
-          Did you receive a referral?:
-          <input type="text" name="referall"/><br/>
-          Did you receive an offer?:
-          <input type="text" name="offer"/><br/>
-          Interview Notes:
-          <input type="text" name="interviewNotes"/><br/>
-          Difficulty Rating:
-          <input type="text" name="difficultyRating"/><br/> 
-          Experience Rating:
-          <input type="text" name="experienceRating"/><br/> 
-          Offer Accepted?:
-          <input type="text" name="offer"/><br/> 
-          Did you have a phone screen?:
-          <input type="text" name="phoneScreen"/><br/> 
+          Company:
+          <input type="text" name="company"/><br/>
+          
+          How difficult was the interview overall? 
+          <input type="radio" name="difficultyRating" value="easy"/> Easy <input type="radio" name="difficultyRating" value="medium"/> Medium <input type="radio" name="difficultyRating" value="hard"/> Hard<br/>
+
+          How was your experience overall?
+          <input type="radio" name="experienceRating"value="positive"/> Positive <input type="radio" name="experienceRating" value="neutral"/> Neutral <input type="radio" name="experienceRating" value="negative"/> Negative<br/>
+
+          Did you receive a referral?  
+          <input type="radio" name="referral" value="yes"/> Yes <input type="radio" name="referral" value="no"/> No<br/>
+
+
+          Did you receive an offer? 
+          <input type="radio" name="offer" value="yes"/> Yes <input type="radio" name="offer" value="no"/> No<br/>
+
+          Did you accept an offer? 
+          <input type="radio" name="offerAccepted" value="yes"/> Yes <input type="radio" name="phoneScreen" value="no"/> No<br/>
+
+          Which of the following elements of the interview did you complete?<br/>
+          Phone screen <input type="checkbox" name="phoneScreen"/><br/>
+          Tech screen <input type="checkbox" name="techScreen"/><br/>
+          Take-home challenge <input type="checkbox" name="takeHomeChallenge"/><br/>
+          On-site interview <input type="checkbox" name="takeHomeChallenge"/><br/>
+          Whiteboarding challenge <input type="checkbox" name="whiteboardChallenge"/><br/>
+
+
+
           Phone screen details:
           <input type="text" name="phoneScreenDetails"/><br/> 
-          Did you have a tech screen?:
-          <input type="text" name="techScreen"/><br/> 
+
+          Did you have a tech screen? 
+          <input type="radio" name="techScreen" value="yes"/> Yes <input type="radio" name="techScreen" value="no"/> No<br/> 
           Tech screen details:
           <input type="text" name="techScreenDetails"/><br/>
-          Did you have a take home challenge?
-          <input type="text" name="takeHomeChallenge"/><br/>
+          
+          Did you have a take-home challenge? 
+          <input type="radio" name="takeHomeChallenge" value="yes"/> Yes <input type="radio" name="takeHomeChallenge" value="no"/> No<br/>
           Take-home challenge details:
           <input type="text" name="takeHomeChallengeDetails"/><br/>
-          Did you have an onsite interview?
-          <input type="text" name="onsiteInterview"/><br/>
-          Onsite interview details:
+
+          Did you have an on-site interview? 
+          <input type="radio" name="takeHomeChallenge" value="yes"/> Yes <input type="radio" name="takeHomeChallenge" value="no"/> No<br/>
+          On-site interview details:
           <input type="text" name="onsiteInterviewDetails"/><br/>
-          Did you have a whiteboard challenge?:
-          <input type="text" name="whiteboardChallenge"/><br/>
+
+          Did you have a whiteboard challenge? 
+          <input type="radio" name="whiteboardChallenge" value="yes"/> Yes <input type="radio" name="whiteboardChallenge" value="no"/> No<br/>
           Whiteboard challenge details:
           <input type="text" name="whiteboardChallengeDetails"/><br/>
+
           Negotiations Details:
           <input type="text" name="negotiationsDetails"/><br/>
+          Additional Interview Notes:
+          <input type="text" name="interviewNotes"/><br/>
           
-          <button className="submit">submit</button>
-      </div>
+          <button className="submit">submit</button><br/>
+      </form>
     )
   }
 }
