3542579bd70c243229d90e62294abd3efd60eb9a
flash00 <dexlorraine@gmail.com>
2017-12-05 18:07:11 -0800

    Create searchBox and searchResults component.

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -4,7 +4,7 @@ import InterviewList from './Interviews/InterviewList'
 import CompanyList from './Companies/CompanyList'
 import HomePage from './HomePage'
 import InterviewForm from './Interviews/InterviewForm'
-import CompanySearchForm from './Companies/CompanySearchForm'
+import SearchBox from './Searches/searchBox'
 import UserProfile from './Users/UserProfile'
 
 
@@ -17,7 +17,7 @@ class Main extends Component {
           <Route exact path='/interviews/new' component={InterviewForm} />
           <Route exact path='/companies' component={CompanyList} />
           <Route exact path='/' component={HomePage} />
-          <Route exact path='/search' component={CompanySearchForm} />
+          <Route exact path='/search' component={SearchBox} />
           <Route exact path='/users/:id' component={UserProfile} />
 
         </Switch>
