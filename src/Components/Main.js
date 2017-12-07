a927ddf11858b45eea8f0b0eede95e1c32c90f17
flash00 <dexlorraine@gmail.com>
2017-12-07 15:18:18 -0800

    Adjust search box.

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -6,19 +6,9 @@ import HomePage from './HomePage'
 import InterviewForm from './Interviews/InterviewForm'
 import SearchBox from './Searches/searchBox'
 import UserProfile from './Users/UserProfile'
-<<<<<<< HEAD
-<<<<<<< HEAD
-import CompanyProfile from './Companies/CompanyProfile'
-import UserLogin from './Login/userLogin'
-=======
-import CompanyItemDetail from './Companies/CompanyItemDetail'
->>>>>>> f0babea8bcb5d2cdd118b5fb524f8077a79d1b8d
-
-=======
-import CompanyProfile from './Companies/CompanyProfile'
 import UserLogin from './Login/userLogin'
 import CompanyItemDetail from './Companies/CompanyItemDetail'
->>>>>>> f2195bb0ea44d1f5f6aee61ec70a6b236fa6f75c
+import CompanyProfile from './Companies/CompanyProfile'
 
 class Main extends Component {
 
@@ -32,17 +22,10 @@ class Main extends Component {
           <Route exact path='/' component={HomePage} />
           <Route exact path='/search' component={SearchBox} />
           <Route exact path='/users/:id' component={UserProfile} />
-<<<<<<< HEAD
-<<<<<<< HEAD
           <Route exact path='/companies/:id' component={CompanyProfile} />
           <Route exact path='/login' component={UserLogin} />
-=======
           <Route exact path='/companies/:id' component={CompanyItemDetail} />
->>>>>>> f0babea8bcb5d2cdd118b5fb524f8077a79d1b8d
-=======
           <Route exact path='/login' component={UserLogin} />
-          <Route exact path='/companies/:id' component={CompanyItemDetail} />
->>>>>>> f2195bb0ea44d1f5f6aee61ec70a6b236fa6f75c
         </Switch>
       </div>
     )
--- src/Components/Searches/searchBox.js
+++ src/Components/Searches/searchBox.js
@@ -48,11 +48,9 @@ class SearchBox extends React.Component {
     var companyName = this.state.companyName;
     return (
       <div>
-        <div className='row'>
+        <div className='search'>
           <form onSubmit={this.handleSubmit}>
-              <h5>Search:
-              <input type='text' id='search' name='search_form_input' value={this.state.searchTerm} onChange={this.handleChange} />
-              </h5>
+              <input type='text' placeholder=' Search for company...' name='search_form_input' value={this.state.searchTerm} onChange={this.handleChange} />
             <input type="submit" id="submit" value="Submit" />
           </form>
         </div>
