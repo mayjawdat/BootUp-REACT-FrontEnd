f0babea8bcb5d2cdd118b5fb524f8077a79d1b8d
Kenn Sugiyama <kenn@kbsugiyama.com>
2017-12-06 22:37:25 -0800

    Display location and website for company in company Detail view.

--- src/Components/Interviews/InterviewListItem.js
+++ src/Components/Interviews/InterviewListItem.js
@@ -3,7 +3,7 @@ import React from 'react';
 const InterviewListItem = ({interview}) => {
     return (
         <div>
-            <li>{interview.interviewee.name}'s interview</li>
+            <li>Interview for {interview.job_title} position</li>
         </div>
     );
 };
