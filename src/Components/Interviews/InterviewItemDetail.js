ce5b4539ab0c64476b0920e628f5e82bd4f1a58c
MattBootPro <mattbootpro@MattBootPros-MacBook-Pro.local>
2017-12-07 15:24:59 -0800

    Enable companies interviews to link to interview show

--- src/Components/Interviews/InterviewItemDetail.js
+++ src/Components/Interviews/InterviewItemDetail.js
@@ -1,38 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-const InterviewItemDetail = ({selectedInterview}) => {
-    console.log(selectedInterview.interviewee)
-    if (selectedInterview.interviewee) {
-        return (
-            <div>
-                <p>{selectedInterview.interviewee.name}</p>
-                <p>{selectedInterview.company.name}</p>
-                <p>{selectedInterview.job_title}</p>
-                <p>{selectedInterview.referred}</p>
-                <p>{selectedInterview.received_offer}</p> 
-                <p>{selectedInterview.notes}</p>
-                <p>{selectedInterview.difficulty_rating}</p>
-                <p>{selectedInterview.experience_rating}</p>
-                <p>{selectedInterview.accepted_offer}</p>
-                <p>{selectedInterview.phone_screen}</p>
-                <p>{selectedInterview.phone_screen_details}</p>
-                <p>{selectedInterview.tech_screen}</p>
-                <p>{selectedInterview.tech_screen_details}</p>
-                <p>{selectedInterview.take_home_challenge}</p>
-                <p>{selectedInterview.take_home_challenge_details}</p>
-                <p>{selectedInterview.onsite}</p>
-                <p>{selectedInterview.onsite_details}</p>
-                <p>{selectedInterview.whiteboarding}</p>
-                <p>{selectedInterview.whiteboarding_details}</p>
-                <p>{selectedInterview.negotiation_details}</p><br/>
-            </div>
-        );
-    } else {
-        return (
-            <div></div>
-        )
+class InterviewItemDetail extends Component {
+    constructor(props){
+        super()
+
+        this.state = {
+            interview: {}
+        }
     }
 
-};
+    componentDidMount(){
+        axios.get(`/api/interviews/${this.props.match.params.id}`)
+            .then(({data}) => this.setState({interview: data}))
+    }
+
+    render() {
+        return (
+            <div className="interview-item-detail">
+                <p> {this.state.interview.job_title} </p>
+            </div>
+        );
+    }
+}
 
 export default InterviewItemDetail;
---
+++ src/Components/Interviews/InterviewProfile.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import InterviewItemDetail from './InterviewItemDetail'
+import axios from 'axios'
+
+class CompanyProfile extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+
+    }
+  }
+}
