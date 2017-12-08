68c27eebdefd864806dfd840d1d542a1bf6e0d57
flash00 <dexlorraine@gmail.com>
2017-12-08 08:19:35 -0800

    Style new interview form and search submit button.

--- src/Components/Interviews/InterviewForm.js
+++ src/Components/Interviews/InterviewForm.js
@@ -20,7 +20,7 @@ class InterviewForm extends Component {
     })
     .then((response) => console.log(response));
   }
-  
+
 
   render() {
     return (
@@ -44,6 +44,7 @@ class InterviewForm extends Component {
                       <Radio group={group} value="2" id="average" className="d-inline-block" />
                       <label htmlFor="difficult" className="mr-2">Difficult</label>
                       <Radio group={group} value="3" id="difficult" className="d-inline-block" />
+                      <span></span>
                     </div>
                   )}
                 </RadioGroup>
@@ -51,12 +52,13 @@ class InterviewForm extends Component {
                   { group => (
                     <div>
                       How was your overall experience with this company? <br />
-                      <label htmlFor="negative" className="mr-2">negative</label>
+                      <label htmlFor="negative" className="mr-2">Negative</label>
                       <Radio group={group} value="1" id="negative" className="mr-3 d-inline-block" />
                       <label htmlFor="neutral" className="mr-2">Neutral</label>
                       <Radio group={group} value="2" id="neutral" className="d-inline-block" />
                       <label htmlFor="positive" className="mr-2">Positive</label>
                       <Radio group={group} value="3" id="positive" className="d-inline-block" />
+                      <span></span>
                     </div>
                   )}
                 </RadioGroup>
@@ -68,6 +70,7 @@ class InterviewForm extends Component {
                       <Radio group={group} value="1" id="yes" className="mr-3 d-inline-block" />
                       <label htmlFor="no" className="mr-2">No</label>
                       <Radio group={group} value="0" id="no" className="d-inline-block" />
+                      <span></span>
                     </div>
                   )}
                 </RadioGroup>
@@ -79,6 +82,7 @@ class InterviewForm extends Component {
                       <Radio group={group} value="1" id="yes" className="mr-3 d-inline-block" />
                       <label htmlFor="no" className="mr-2">No</label>
                       <Radio group={group} value="0" id="no" className="d-inline-block" />
+                      <span></span>
                     </div>
                   )}
                 </RadioGroup>
@@ -90,49 +94,50 @@ class InterviewForm extends Component {
                       <Radio group={group} value="1" id="yes" className="mr-3 d-inline-block" />
                       <label htmlFor="no" className="mr-2">No</label>
                       <Radio group={group} value="0" id="no" className="d-inline-block" />
+                      <span></span>
                     </div>
                   )}
                 </RadioGroup>
 
-                
-                Please detail the steps of the interview process.<br/>
+
+                Please detail the steps of the interview process.<br />
                 <label htmlFor="phoneScreen" className="mr-2">Phone screen</label>
-                <Checkbox field="phoneScreen" id="phoneScreen" className="d-inline-block" /><br/>
+                <Checkbox field="phoneScreen" id="phoneScreen" className="d-inline-block" /> <span></span>
 
                 <label htmlFor="techScreen" className="mr-2">Tech screen</label>
-                <Checkbox field="techScreen" id="techScreen" className="d-inline-block" /><br/>
+                <Checkbox field="techScreen" id="techScreen" className="d-inline-block" /> <span></span>
 
                 <label htmlFor="takeHomeChallenge" className="mr-2">Take-home challenge</label>
-                <Checkbox field="takeHomeChallenge" id="takeHomeChallenge" className="d-inline-block" /><br/>
+                <Checkbox field="takeHomeChallenge" id="takeHomeChallenge" className="d-inline-block" /> <span></span>
 
                 <label htmlFor="onsiteInterview" className="mr-2">On-site interview</label>
-                <Checkbox field="onsiteInterview" id="onsiteInterview" className="d-inline-block" /><br/>
+                <Checkbox field="onsiteInterview" id="onsiteInterview" className="d-inline-block" /> <span></span>
 
                 <label htmlFor="whiteboardChallenge" className="mr-2">Whiteboarding challenge</label>
-                <Checkbox field="whiteboardChallenge" id="whiteboardChallenge" className="d-inline-block" /><br/>
+                <Checkbox field="whiteboardChallenge" id="whiteboardChallenge" className="d-inline-block" /> <span></span><br />
 
-                Please elaborate as needed.<br/>
+                Please elaborate as needed.<br />
                 <label htmlFor="phone_screen_details">Phone screen details:</label>
-                <StyledTextArea field="phone_screen_details" id="phone_screen_details" /><br/>
-                
-                <label htmlFor="tech_screen_details">Tech screen details:</label>
-                <StyledTextArea field="tech_screen_details" id="tech_screen_details" /><br/>
+                <StyledTextArea field="phone_screen_details" id="phone_screen_details" /><span></span>
+
+                <label htmlFor="tech_screen_details">     Tech screen details:</label>
+                <StyledTextArea field="tech_screen_details" id="tech_screen_details" /><span></span>
 
                 <label htmlFor="take_home_challenge_details">Take-home challenge details:</label>
-                <StyledTextArea field="take_home_challenge_details" id="take_home_challenge_details" /><br/>
-                
+                <StyledTextArea field="take_home_challenge_details" id="take_home_challenge_details" /><span></span>
+
                 <label htmlFor="onsite_details">On-site interview details:</label>
-                <StyledTextArea field="onsite_details" id="onsite_details" /><br/>
-                
+                <StyledTextArea field="onsite_details" id="onsite_details" /><span></span>
+
                 <label htmlFor="whiteboarding_details">Whiteboarding challenge details:</label>
-                <StyledTextArea field="whiteboarding_details" id="whiteboarding_details" /><br/>
-                
+                <StyledTextArea field="whiteboarding_details" id="whiteboarding_details" /><span></span>
+
                 <label htmlFor="negotiation_details">Negotiation details:</label>
-                <StyledTextArea field="negotiation_details" id="negotiation_details" /><br/>
-                
+                <StyledTextArea field="negotiation_details" id="negotiation_details" /><span></span>
+
                 <label htmlFor="notes">Additional interview notes:</label>
-                <StyledTextArea field="notes" id="notes" /><br/><br/>
-                
+                <StyledTextArea field="notes" id="notes" /><br />
+
                 <button type="submit" className="mb-4 btn btn-primary">Submit</button>
             </form>
           )}
