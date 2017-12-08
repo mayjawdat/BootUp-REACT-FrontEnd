3ca0e536ebdae6362d92e5b77a9a4c1b5133b585
Jenna Ritten <jritten@umich.edu>
2017-12-08 13:06:18 -0800

    finished styling stuff

--- src/Components/Interviews/InterviewItemDetail.js
+++ src/Components/Interviews/InterviewItemDetail.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import greenPwrBtn from '../../Images/greenPwrBtn.png'
-import redPwrBtn from '../../Images/redPwrBtn.png'
 import yellowPwrBtn from '../../Images/yellowPwrBtn.png'
+import redPwrBtn from '../../Images/redPwrBtn.png'
 
 const experienceRatings = ['Poor', 'Neutral', 'Good']
 const difficultyRatings = ['Easy', 'Medium', 'Hard']
@@ -14,52 +14,53 @@ class InterviewItemDetail extends Component {
     this.state = {
       interview: {
         company: {}
-      }
     }
-  }
+}
+}
 
-  componentDidMount(){
+componentDidMount(){
     axios.get(`/api/interviews/${this.props.match.params.id}`)
     .then(({data}) => this.setState({interview: data}))
-  }
+}
 
-    render() {
-        return (
-            <div className="interview-item-detail">
-                <div className="page-header">
-                    <h2>{this.state.interview.job_title}</h2>
+render() {
+    return (
+        <div className="interview-item-detail">
+            <div className="page-header">
+                <h2>{this.state.interview.job_title}</h2>
 
-                    <p>{this.state.interview.created_at}</p>
                 </div>
                 <div className="interview-list">
-                    <ul>
-                        {this.state.interview.referred && <li><img id="logo" src={greenPwrBtn} /> Referred</li>}
-                        {this.state.interview.accepted_offer && <li><img id="logo" src={greenPwrBtn} /> Accepted Offer</li>}
-                        {!this.state.interview.accepted_offer && <li><img id="logo" src={redPwrBtn} /> Declined Offer</li>}
-                        {this.state.interview.notes && <li>Notes: {this.state.interview.notes} </li>}
-                        {this.state.interview.difficulty_rating === 1 && <li><img id="logo" src={greenPwrBtn} /> Easy Interview</li>}
-                        {this.state.interview.difficulty_rating === 2 && <li><img id="logo" src={yellowPwrBtn} /> Challenging Interview</li>}
-                        {this.state.interview.difficulty_rating === 3 && <li><img id="logo" src={redPwrBtn} /> Difficult Interview</li>}
-                        {this.state.interview.experience_rating === 1 && <li><img id="logo" src={redPwrBtn} /> Negative Experience</li>}
-                        {this.state.interview.experience_rating === 2 && <li><img id="logo" src={yellowPwrBtn} /> Neutral Experience</li>}
-                        {this.state.interview.experience_rating === 3 && <li><img id="logo" src={greenPwrBtn} /> Positive Experience</li>}
-                        {this.state.interview.phone_screen && <h3>Phone Screen</h3>}
-                        {this.state.interview.phone_screen && <li>{this.state.interview.phone_screen_details} </li>}
-                        {this.state.interview.tech_screen && <h3>Tech Screen</h3>}
-                        {this.state.interview.tech_screen && <li>{this.state.interview.tech_screen_details} </li>}
-                        {this.state.interview.take_home_challenge && <h3>Take Home Challenge</h3>}
-                        {this.state.interview.take_home_challenge && <li>{this.state.interview.take_home_challenge_details} </li>}
-                        {this.state.interview.onsite && <h3>On Site Interview</h3>}
-                        {this.state.interview.onsite && <li>{this.state.interview.onsite_details} </li>}
-                        {this.state.interview.whiteboarding && <h3>Whiteboarding</h3>}
-                        {this.state.interview.whiteboarding && <li>{this.state.interview.whiteboarding_details} </li>}
-                        {this.state.interview.negotiation_details && <h3>Negotiation</h3>}
-                        {<li>{this.state.interview.negotiation_details} </li>}
-                    </ul>
-                </div>
+                <ul>
+                {this.state.interview.referred && <span id="logo-desc">  <img id="logo" src={greenPwrBtn} />  Referred  </span>}
+                {!this.state.interview.referred && <span id="logo-desc"><img id="logo" src={yellowPwrBtn} />  No Referral  </span>}
+                {this.state.interview.accepted_offer && <span id="logo-desc"><img id="logo" src={greenPwrBtn} />  Accepted Offer  </span>}
+                {!this.state.interview.accepted_offer && <span id="logo-desc">  <img id="logo" src={redPwrBtn} />  Declined Offer</span>}
+                {this.state.interview.experience_rating === 1 && <span id="logo-desc">  <img id="logo" src={redPwrBtn} />  Negative Experience  </span>}
+                {this.state.interview.experience_rating === 2 && <span id="logo-desc">  <img id="logo" src={yellowPwrBtn} />  Neutral Experience  </span>}
+                {this.state.interview.experience_rating === 3 && <span id="logo-desc">  <img id="logo" src={greenPwrBtn} />  Positive Experience  </span>}
+                {this.state.interview.difficulty_rating === 1 && <span id="logo-desc">  <img id="logo" src={greenPwrBtn} />  Easy Interview  </span>}
+                {this.state.interview.difficulty_rating === 2 && <span id="logo-desc">  <img id="logo" src={yellowPwrBtn} />  Average Interview  </span>}
+                {this.state.interview.difficulty_rating === 3 && <span id="logo-desc">  <img id="logo" src={redPwrBtn} />  Difficult Interview  </span>}
+                {this.state.interview.phone_screen && <h3>Phone Screen</h3>}
+                {this.state.interview.phone_screen && <li>{this.state.interview.phone_screen_details} </li>}
+                {this.state.interview.tech_screen && <h3>Tech Screen</h3>}
+                {this.state.interview.tech_screen && <li>{this.state.interview.tech_screen_details} </li>}
+                {this.state.interview.take_home_challenge && <h3>Take Home Challenge</h3>}
+                {this.state.interview.take_home_challenge && <li>{this.state.interview.take_home_challenge_details} </li>}
+                {this.state.interview.onsite && <h3>On Site Interview</h3>}
+                {this.state.interview.onsite && <li>{this.state.interview.onsite_details} </li>}
+                {this.state.interview.whiteboarding && <h3>Whiteboarding</h3>}
+                {this.state.interview.whiteboarding && <li>{this.state.interview.whiteboarding_details} </li>}
+                {this.state.interview.negotiation_details && <h3>Negotiation</h3>}
+                {this.state.interview.negotiation_details && <li>{this.state.interview.negotiation_details} </li>}
+                {this.state.interview.notes && <li><h3>Additional Notes</h3></li>}
+                {this.state.interview.notes && <li>{this.state.interview.notes} </li>}
+                </ul>
             </div>
+        </div>
         );
-      }
-    }
+}
+}
 
 export default InterviewItemDetail;
--- src/Components/Interviews/InterviewListItem.js
+++ src/Components/Interviews/InterviewListItem.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import greenPwrBtn from '../../Images/greenPwrBtn.png'
+import yellowPwrBtn from '../../Images/yellowPwrBtn.png'
+import redPwrBtn from '../../Images/redPwrBtn.png'
 
 const InterviewListItem = ({interview}) => {
     return (
         <div>
-            <li>
+            <ul className="company-interview-list">
               <Link to={`/interviews/${interview.id}`}>
-               {interview.job_title} at {interview.company.name} 
+              <br />
+                <li>{interview.job_title} at {interview.company.name}</li> 
+                {interview.referred && <span id="logo-desc">  <img id="logo" src={greenPwrBtn} />  Referred  </span>}
+                {!interview.referred && <span id="logo-desc">  <img id ="logo" src={yellowPwrBtn} />  No Referral  </span>}
+                {interview.experience_rating === 1 && <span id="logo-desc">  <img id="logo" src={redPwrBtn} />  Experience  </span>}
+                {interview.experience_rating === 2 && <span id="logo-desc">  <img id="logo" src={yellowPwrBtn} />  Experience  </span>}
+                {interview.experience_rating === 3 && <span id="logo-desc">  <img id="logo" src={greenPwrBtn} />  Experience  </span>}
+                {interview.difficulty_rating === 1 && <span id="logo-desc">  <img id="logo" src={greenPwrBtn} />  Interview  </span>}
+                {interview.difficulty_rating === 2 && <span id="logo-desc">  <img id="logo" src={yellowPwrBtn} />  Interview  </span>}
+                {interview.difficulty_rating === 3 && <span id="logo-desc">  <img id="logo" src={redPwrBtn} />  Interview  </span>}
               </Link>
-            </li>
+            </ul>
         </div>
     );
 };
