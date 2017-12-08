3c90cdd32a89b4ff2184de92aa7a67a7aebfd1a1
Jenna Ritten <jritten@umich.edu>
2017-12-07 23:21:36 -0800

    styling progress

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -5,12 +5,19 @@ import axios from 'axios'
 class HomePage extends Component {
   render() {
     return (
+      <div>
       <div className="homepage">
-        <div className="page-header">
-          <h2>Welcome</h2>
+        <ul>
+          <br />
+          <li><h2>Welcome to BootUp!</h2></li>
+          <li>DBC Bootup lets Dev Bootcamp graduates share their experiences interviewing for web development jobs, helping other boots get insight and a leg up on the interview process. Have an interview coming up? See what advice other boots have while interviewing at that company. Gone through some interviews? Share your experience with the DBC community.</li>
+        </ul>
+      </div>
+        <br />
+        <br />
+        <div id="linkedin-login">
+        <a href="/api/auth/linkedin" className='link-button'>Login with LinkedIn to Get Started</a>
         </div>
-        <p>DBC Bootup lets Dev Bootcamp graduates share their experiences interviewing for web development jobs, helping other boots get insight and a leg up on the interview process. Have an interview coming up? See what advice other boots have while interviewing at that company. Gone through some interviews? Share your experience with the DBC community.</p>
-        <a href="/api/auth/linkedin" className='link-button'>Login with LinkedIn to get started</a>
       </div>
     )
   }
