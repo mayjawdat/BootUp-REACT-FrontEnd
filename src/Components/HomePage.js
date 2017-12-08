2a0574e2e78167cb08b2fa8c70f58643b8ca680c
Josh Leichtung <joshleichtung@gmail.com>
2017-12-08 01:00:04 -0800

    Style profile to fit mockups

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -7,14 +7,11 @@ class HomePage extends Component {
     return (
       <div>
       <div className="homepage">
-        <ul>
-          <br />
-          <li><h2>Welcome to BootUp!</h2></li>
-          <li>DBC Bootup lets Dev Bootcamp graduates share their experiences interviewing for web development jobs, helping other boots get insight and a leg up on the interview process. Have an interview coming up? See what advice other boots have while interviewing at that company. Gone through some interviews? Share your experience with the DBC community.</li>
-        </ul>
+        <div className='page-header'>
+          <h2>Welcome to BootUp!</h2>
+        </div>
+          <p>DBC Bootup lets Dev Bootcamp graduates share their experiences interviewing for web development jobs, helping other boots get insight and a leg up on the interview process. Have an interview coming up? See what advice other boots have while interviewing at that company. Gone through some interviews? Share your experience with the DBC community.</p>
       </div>
-        <br />
-        <br />
         <div id="linkedin-login">
         <a href="/api/auth/linkedin" className='link-button'>Login with LinkedIn to Get Started</a>
         </div>
--- src/Components/Users/UserProfile.js
+++ src/Components/Users/UserProfile.js
@@ -42,11 +42,9 @@ class UserProfile extends Component {
   }
 
   userSkills() {
-    return this.state.user.skills.map(
-      skill => (
-        <span> {skill.name} </span>
-      )
-    )
+    return this.state.user.skills.map( skill => ( 
+      <span> {skill.name} </span>
+    ))
   }
 
   interviewItems() {
@@ -63,21 +61,30 @@ class UserProfile extends Component {
         <div id="photo">
              <img id="logo" src={this.state.user.photo_url ? this.state.user.photo_url : defaultProfile } />
         </div>
-        <h2>{this.state.user.name}</h2>
-        <ul>
-          <li>{this.state.user.current_position} at {this.state.user.current_company}</li>
-          <li>{this.state.user.cohort} {this.state.user.year} - {this.state.user.location}</li>
-            {this.userSkills()}
-            <li><a href={`mailto:${this.state.user.email}`}>{this.state.user.email}</a></li>
+        <h2 className="profile-name">{this.state.user.name}</h2>
+        <h3>{this.state.user.current_position}</h3>
+        <h4>at {this.state.user.current_company}</h4>
+        <div className='profile-cohort'>
+          {this.state.user.cohort && `${this.state.user.year} ${this.state.user.cohort}`} {this.state.user.year} {this.state.user.cohort && ' - '}{this.state.user.location}
+        </div>
+        <div className='profile-email'>
+          <a href={`mailto:${this.state.user.email}`}>{this.state.user.email}</a>
+        </div>
+        <div className="profile-links">
+            <ul>
             {this.state.user.github_url && <li><a href={this.state.user.github_url} target="_blank"><img id="logo" src={github_logo} /></a></li>}
             {this.state.user.linkedin_url && <li><a href={this.state.user.linkedin_url} target="_blank"><img id="logo" src={linkedin_logo} /></a></li>}
             {this.state.user.facebook_url && <li><a href={this.state.user.facebook_url} target="_blank"><img id="logo" src={fb_logo} /></a></li>}
         </ul>
+      </div>
+        {this.userSkills()}
+        <div className='profile-interview-list'>
         <h3>Interviews</h3>
         <ul>
           {this.interviewItems()}
             <button id="add-button"><Link id="add-interview" to="/interviews/new"><img id="plus" src={plusSign} /> Add Interview</Link></button>
         </ul>
+      </div>
       </div>
     )
   }
