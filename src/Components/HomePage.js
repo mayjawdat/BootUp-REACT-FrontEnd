04dfecacd42d740eb412ee37d3595c2f6701a6f1
May Jawdat <may.jawdat@gmail.com>
2017-12-08 11:19:40 -0800

    Update homepage text

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -10,7 +10,7 @@ class HomePage extends Component {
         <div className='page-header'>
           <h2>Welcome to BootUp!</h2>
         </div>
-          <p>DBC Bootup lets Dev Bootcamp graduates share their experiences interviewing for web development jobs, helping other boots get insight and a leg up on the interview process. Have an interview coming up? See what advice other boots have while interviewing at that company. Gone through some interviews? Share your experience with the DBC community.</p>
+          <p>BootUp is an app that helps DBC grads share information about their interview experiences, providing insights and tips from the unique perspective of another boot. Have an interview coming up? See what alums have to say about their experiences with the company. Gone through the interview process with a company? Let the community know what it was like.</p>
       </div>
         <div id="linkedin-login">
         <a href="/api/auth/linkedin" className='link-button'>Login with LinkedIn to Get Started</a>
