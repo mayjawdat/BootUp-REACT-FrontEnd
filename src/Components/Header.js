1e9f470fb3a83870ed58c9cc3667a005091124f5
Jenna Ritten <jritten@umich.edu>
2017-12-20 22:53:46 -0500

    Changes to HomePage. Fixed bug on InterviewList filter.

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -9,9 +9,10 @@ class Header extends Component {
           <div><Link to="/interviews"><img id="logo" src={BootUpLogo2} /></Link></div>
         <nav>
           <ul id="links">
+            {!this.props.current_user && <li><a href="/">Login/Sign-Up</a></li>}
             {this.props.current_user && <li><Link to={`/users/${this.props.current_user.id}`}>{`${this.props.current_user.name}`}</Link></li>}
             {this.props.current_user && <li><Link to="/companies">Companies</Link></li>}
-            {this.props.current_user && <li><Link to="/">Resources</Link></li>}
+            {this.props.current_user && <li><Link to="/interviews">Resources</Link></li>}
             {/*<li><Link to="/search">Search</Link></li>*/}
             {this.props.current_user && <li><a href='/api/logout'>Logout</a></li>}
           </ul>
